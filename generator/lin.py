4108634f6b503e61983d42ab9104f0b0286b76a5
lee <charismafight@hotmail.com>
2017-12-26 17:02:45 +0800

    init

--- generator/lin.py
+++ generator/lin.py
@@ -6,21 +6,30 @@ class Lin(object):
 
     def __init__(self, file_url):
         self.file_url = file_url
+        self.__content = None
 
     @property
     def content(self):
         """
         :return: a string including file content
         """
-        if self.__content:
-            return self.__content
-        else:
-            # TODO
-            pass
-        pass
+        return self.__content
+
+    @content.setter
+    def content(self, value):
+        self.__content = value
 
     def init_data(self):
         """
         initiate data from a string,and set object's attr
         :return: void
         """
+        pass
+
+    def fetch_file(self):
+        """
+        fetch file by url
+        eg:http://www.bridgebase.com/myhands/fetchlin.php?id=1535437396&when_played=1514118898
+        :return:
+        """
+        pass
